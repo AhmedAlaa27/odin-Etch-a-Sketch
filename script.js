638cd4a0c5166264e901e2a7d930e67731a32886
AhmedAlaa27 <ahmedalaa277143@gmail.com>
2024-08-05 17:28:25 +0300

    Set up a 'hover' effect to change background to black

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const container = document.querySelector(".container")
 
 for (let i = 1; i <= 256; i++) {
+
     let button = document.createElement("div")
     button.classList.toggle("box")
+
+    button.addEventListener("mouseenter", () => {
+        button.style.backgroundColor = "black"
+    })
+
     container.appendChild(button)
 }
